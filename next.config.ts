983ce5f471c34aced96862454dc4a0cbf021e5ad
themagiche <magiche.mc@gmail.com>
2026-09-14 06:24:31 +0300

    prevent post fetch fail

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['0.0.0.0:3000'],
+  outputFileTracingIncludes: {
+    '/*': ['./outstatic/content/**/*'],
+  },
 };
 
 export default nextConfig;
